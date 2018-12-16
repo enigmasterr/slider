5f9cc2135b9aca4a12378106eec6e1027c345259
enigmaster <nigma.corp@gmail.com@>
2018-12-16 10:17:16 +0500

    Интенсив по Реакту - Слайдер

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div class="wrapper">
+            <SayHelloName name="Vadim" aka="Enigmaster" />         
+        </div>    
     );
   }
 }
 
+function SayHelloName(props) {
+  return (
+        <div>
+          <h1>Hello World!</h1>
+          <h2>Мое имя {props.name}, Ник - {props.aka}</h2>
+        </div>
+    )
+}
 export default App;
